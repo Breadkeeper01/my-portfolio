4a35110a792d01c0164e16106368ecf46fcb4817
Prince Obina <princeobina92@gmail.com>
2025-09-24 19:08:17 +0800

    Feature: Navigation bar, hero section and about section

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import NavBar from "./sections/NavBar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <NavBar />
+      <Hero />
+      <About />
+      {/* <section className="min-h-screen" /> */}
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testemonials */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
